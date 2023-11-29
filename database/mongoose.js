721540d25cda8f683cb50968e6e8ca85f1d4b0b3
Prajwal Bati <prajwalbati@gmail.com>
2023-11-28 22:57:16 -0500

    fix connection

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,13 +1,15 @@
 let mongoose = require('mongoose');
 
 let dbConnection = async () => {
-    mongoose.Promise = global.Promise;
-    mongoose.set('returnOriginal', false);
-    await mongoose.connect(process.env.MONGO_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+    try {
+        mongoose.Promise = global.Promise;
+        mongoose.set('returnOriginal', false);
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(process.env.MONGO_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: false });
         console.log("Database connected successfully");
-    }).catch(err => {
+    } catch (err) {
         console.error("***** Error while connecting database: "+err.message+" *****");
-    });
+    }
 };
 
 dbConnection();
